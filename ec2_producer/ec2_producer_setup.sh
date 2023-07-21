ae09fdbabaec46db1c5bd4494ad97692547b1b53
John Peoples <jwp5828@psu.edu>
2023-07-21 12:01:44 -0400

    altered producer setup to work with the Amazon-Linux AMI (i.e., using yum)

--- ec2_producer/ec2_producer_setup.sh
+++ ec2_producer/ec2_producer_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt install python3-pip
+sudo yum update
+sudo yum install python3-pip
 sudo pip install -r requirements.txt
 
 # install java 
-sudo apt install openjdk-17-jdk openjdk-17-jre
+sudo yum install openjdk-17-jdk openjdk-17-jre
